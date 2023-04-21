23cafd0b8ad7a6f9c1488fa44b586f43fd7f0470
Karl Williamson <khw@cpan.org>
2023-04-21 10:53:33 -0600

    Enable DEBUG_SETLOCALE_INCONSISTENCIES

--- perl.h
+++ perl.h
@@ -1326,6 +1326,7 @@ typedef enum {
 /* end of makedef.pl logic duplication
  * ========================================================================= */
 
+#define DEBUG_SETLOCALE_INCONSISTENCIES
 #ifdef DEBUG_SETLOCALE_INCONSISTENCIES
 #  if ! defined(DEBUGGING) || defined(USE_POSIX_2008) || ! defined(USE_LOCALE_CTYPE)
 #    undef DEBUG_SETLOCALE_INCONSISTENCIES
